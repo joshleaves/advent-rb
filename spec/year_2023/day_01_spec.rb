45274669f283d7aa9847f1c7eec44431d79353cc
red <red@dreamleaves.org>
2024-01-31 02:19:13 +0100

    Year 2023: Day 01

---
+++ spec/year_2023/day_01_spec.rb
@@ -0,0 +1,88 @@
+require 'year_2023/day_01'
+
+RSpec.describe Year2023::Day01 do
+  context 'Part 1' do
+    subject do
+      Year2023::Day01.new(File.read('spec/year_2023/day_01_sample_one'), true)
+    end
+
+    it 'picks up the first digit' do
+      expect(subject.lines[0].first_digit).to eq('1')
+      expect(subject.lines[1].first_digit).to eq('3')
+      expect(subject.lines[2].first_digit).to eq('1')
+      expect(subject.lines[3].first_digit).to eq('7')
+    end
+
+    it 'picks up the last digit' do
+      expect(subject.lines[0].last_digit).to eq('2')
+      expect(subject.lines[1].last_digit).to eq('8')
+      expect(subject.lines[2].last_digit).to eq('5')
+      expect(subject.lines[3].last_digit).to eq('7')
+    end
+
+    it 'forms a two-digit number' do
+      expect(subject.lines[0].to_i).to eq(12)
+      expect(subject.lines[1].to_i).to eq(38)
+      expect(subject.lines[2].to_i).to eq(15)
+      expect(subject.lines[3].to_i).to eq(77)
+    end
+
+    it 'gives a final result' do
+      expect(subject.to_i).to eq(142)
+    end
+  end
+
+  context 'Part 2' do
+    subject do
+      Year2023::Day01.new(File.read('spec/year_2023/day_01_sample_two'))
+    end
+
+    it 'picks up the first digit' do
+      expect(subject.lines[0].first_digit).to eq('2')
+      expect(subject.lines[1].first_digit).to eq('8')
+      expect(subject.lines[2].first_digit).to eq('1')
+      expect(subject.lines[3].first_digit).to eq('2')
+      expect(subject.lines[4].first_digit).to eq('4')
+      expect(subject.lines[5].first_digit).to eq('1')
+      expect(subject.lines[6].first_digit).to eq('7')
+    end
+
+    it 'picks up the last digit' do
+      expect(subject.lines[0].last_digit).to eq('9')
+      expect(subject.lines[1].last_digit).to eq('3')
+      expect(subject.lines[2].last_digit).to eq('3')
+      expect(subject.lines[3].last_digit).to eq('4')
+      expect(subject.lines[4].last_digit).to eq('2')
+      expect(subject.lines[5].last_digit).to eq('4')
+      expect(subject.lines[6].last_digit).to eq('6')
+    end
+
+    it 'forms a two-digit number' do
+      expect(subject.lines[0].to_i).to eq(29)
+      expect(subject.lines[1].to_i).to eq(83)
+      expect(subject.lines[2].to_i).to eq(13)
+      expect(subject.lines[3].to_i).to eq(24)
+      expect(subject.lines[4].to_i).to eq(42)
+      expect(subject.lines[5].to_i).to eq(14)
+      expect(subject.lines[6].to_i).to eq(76)
+    end
+
+    it 'gives a final result' do
+      expect(subject.to_i).to eq(281)
+    end
+  end
+
+  context 'Results' do
+    subject do
+      File.read('spec/year_2023/day_01_input')
+    end
+
+    it 'correctly answers part 1' do
+      expect(Year2023::Day01.new(subject, true).to_i).to eq(56049)
+    end
+
+    it 'correctly answers part 2' do
+      expect(Year2023::Day01.new(subject).to_i).to eq(54530)
+    end
+  end
+end
---
+++ year_2023/day_01.rb
@@ -0,0 +1,52 @@
+class Year2023
+  class Day01
+    attr_reader :input, :lines
+
+    class Line
+      def initialize(input_line)
+        @line = input_line
+      end
+
+      def first_digit
+        @line[/\d/]
+      end
+
+      def last_digit
+        @line[/.*(\d).*?/, 1]
+      end
+
+      def to_i
+        [first_digit, last_digit].join.to_i
+      end
+    end
+
+    class LinePartTwo < Line
+      NUMBERS = %w(zero one two three four five six seven eight nine).each_with_index.to_h{|num, i| [num, i.to_s] }.to_h
+
+      def first_digit
+        fd = @line[/\d|one|two|three|four|five|six|seven|eight|nine/]
+        return fd if fd =~ /\d/
+
+        NUMBERS[fd]
+      end
+
+      def last_digit
+        ld = @line[/.*(\d|one|two|three|four|five|six|seven|eight|nine).*?$/, 1]
+        return ld.to_s if ld =~ /\d/
+
+        NUMBERS[ld].to_s
+      end
+    end
+
+    def initialize(input_file, part_one = false)
+      @input = input_file
+      @lines = input_file.chomp.split("\n").map do |input_line|
+        (part_one ? Line : LinePartTwo).new(input_line)
+      end
+    end
+
+    def to_i
+      @lines.map(&:to_i).inject(&:+)
+    end
+  end
+end
